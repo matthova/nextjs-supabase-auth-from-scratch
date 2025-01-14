45d55cb251a63d5e746667d604483e5084e33c85
Matt Hova <matthova87@gmail.com>
2025-01-13 16:27:24 -0800

    cleanup

--- src/app/(app)/PageClient.tsx
+++ src/app/(app)/PageClient.tsx
@@ -9,27 +9,50 @@ interface PageClientProps {
   count: number | null;
 }
 export function PageClient({ count }: PageClientProps) {
+  const [error, setError] = React.useState<string | null>(null);
   const user = useAppSelector((state) => userSelectors.getUser(state));
   const [optimisticCount, setOptimisticCount] = React.useOptimistic(
     count ?? 0,
     (state, newVal: number) => newVal
   );
 
-  function handleSubmit() {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setError(null);
     if (user == null) return;
-    setOptimisticCount(optimisticCount + 1);
-    incrementCount(user.id);
+    React.startTransition(() => {
+      setOptimisticCount(optimisticCount + 1);
+    });
+    incrementCount(user.id).catch(() => {
+      React.startTransition(() => {
+        setOptimisticCount(optimisticCount);
+      });
+      setError("Error incrementing count");
+    });
   }
 
   return (
     <div>
       <h1>Hello, world!</h1>
-      <div>{JSON.stringify(user)}</div>
       <div>Count: {optimisticCount}</div>
-      <form action={handleSubmit}>
-        <input type="hidden" name="userId" value={user?.id} />
-        <button type="submit">Increment count</button>
+      <form onSubmit={handleSubmit}>
+        <button
+          type="submit"
+          className="px-2 py-1 border border-solid border-foreground rounded-xl"
+        >
+          Increment count
+        </button>
       </form>
+      {error == null ? null : <div className="text-red-500">{error}</div>}
+      <br />
+      <h2>User Info</h2>
+      <div>
+        {Object.entries(user ?? {}).map(([key, value]) => (
+          <div key={key}>
+            {key}: {JSON.stringify(value)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/app/actions/incrementCount.ts
+++ src/app/actions/incrementCount.ts
@@ -4,6 +4,21 @@ import { incrementCountForUser } from "@/db/queries";
 import { revalidatePath } from "next/cache";
 
 export async function incrementCount(userId: string) {
-  await incrementCountForUser(userId);
-  revalidatePath("/");
+  let didError = false;
+  try {
+    const rand = Math.random();
+    if (rand > 0.5) {
+      throw new Error("ope");
+    }
+    await incrementCountForUser(userId);
+  } catch (ex) {
+    console.log("Increment failed", ex);
+    didError = true;
+  } finally {
+    revalidatePath("/");
+  }
+
+  if (didError) {
+    throw new Error("Increment failed");
+  }
 }
